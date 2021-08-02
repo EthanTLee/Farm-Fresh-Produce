f19b331e0453e9a8799cb9872da5279da8bb5d61
EthanTLee <tethanlee@gmail.com>
2021-08-01 20:33:39 -1000

    bake

--- kitchen.py
+++ kitchen.py
@@ -1,10 +1,10 @@
 from types import SimpleNamespace
 
 kitchen = SimpleNamespace()
-kitchen.ingredient_box = SimpleNamespace()
+kitchen.__setattr__('ingredient box', SimpleNamespace())
 
 def add_ingredients(**kwargs):
-    kitchen.ingredient_box.__dict__.update(kwargs)
+    kitchen.__dict__['ingredient box'].__dict__.update(kwargs)
 
 def add_empty_bowls(*args):
     for arg in args: kitchen.__setattr__(arg, SimpleNamespace())
@@ -13,9 +13,16 @@ def move(name, origin, destination):
     kitchen.__dict__[destination].__setattr__(name, kitchen.__dict__[origin].__dict__[name])
     kitchen.__dict__[origin].__delattr__(name)
 
-def combine(*items, location, new_name):
+def mix(*items, location, new_name):
     kitchen.__dict__[location].__setattr__(new_name, list(map(lambda n: kitchen.__dict__[location].__dict__[n], items)))
     for item in items: kitchen.__dict__[location].__delattr__(item)
+
+def bake(bowl, function, new_name):
+    result = function(kitchen.__dict__[bowl])    
+    kitchen.__delattr__(bowl)
+    kitchen.__setattr__(bowl, SimpleNamespace())
+    kitchen.__dict__[bowl].__setattr__(new_name, result)
+
 
 
     
--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 from kitchen import *
 
-add_empty_bowls('pie_tin', 'mixing_bowl')
-add_ingredients(apple='granny smith', sugar='brown sugar')
-move('apple', origin='ingredient_box', destination='mixing_bowl')
-move('sugar', origin='ingredient_box', destination='mixing_bowl')
-combine('apple', 'sugar', location='mixing_bowl', new_name='apple pie goo filling')
+add_empty_bowls('pie tin', 'mixing bowl')
+add_ingredients(apple='granny smith', sugar='brown sugar', crust='crispy crust')
+move('apple', origin='ingredient box', destination='mixing bowl')
+move('sugar', origin='ingredient box', destination='mixing bowl')
+mix('apple', 'sugar', location='mixing bowl', new_name='apple pie goo filling')
+move('apple pie goo filling', origin='mixing bowl', destination='pie tin')
+move('crust', origin='ingredient box', destination='pie tin')
+bake('pie tin', function=lambda b: 'apple pie' if b.__dict__['crust'] and b.__dict__['apple pie goo filling'] else 'gross pie', new_name='pie')
 
 print('done')
